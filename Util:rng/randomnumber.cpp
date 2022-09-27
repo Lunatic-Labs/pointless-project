bec64f6b5c4879e6b332ce64d9cf65b2b9f9a1eb
connoree <connoreeaton@gmail.com>
2022-09-27 16:06:32 -0500

    Random Number Generator Update
    Added minimum and maximum parameters and removed main

--- Util:rng/randomnumber.cpp
+++ Util:rng/randomnumber.cpp
@@ -3,16 +3,8 @@
 #include <ctime>
 using namespace std;
 
-int getNum(){
+int getNum(int min, int max){
     unsigned int seed = static_cast<unsigned int>(time(nullptr));
     srand(seed);
-    return rand() % 10000000;
-}
-int main()
-{
-    int num;
-    for(int i = 0; i < 8; ++i){
-        num = getNum();
-        cout << num << " ";
-    }
+    return rand() % max + min;
 }
