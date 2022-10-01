81758621653c8e37248c42ea05400c444242b26a
connoree <connoreeaton@gmail.com>
2022-10-01 14:52:46 -0500

    Update Random Number Generator
    Fixed the range problem

--- Util:rng/randomnumber.cpp
+++ Util:rng/randomnumber.cpp
@@ -6,5 +6,5 @@ using namespace std;
 int getNum(int min, int max){
     unsigned int seed = static_cast<unsigned int>(time(nullptr));
     srand(seed);
-    return rand() % max + min;
+    return rand() % (max - min) + min;
 }
