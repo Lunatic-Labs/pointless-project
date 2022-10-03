49cdcaace30bb33098b0cba847b512ec4548d227
connoree <connoreeaton@gmail.com>
2022-10-03 16:40:32 -0500

    Update to randomnumber.cpp
    Added functionality to pass test cases: the entered parameters were the same, the entered parameter for the min value was larger than the max (and vice versa)

--- util/rng/randomnumber.cpp
+++ util/rng/randomnumber.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-using namespace std;
+#include <cassert>
 
 int getNum(int min, int max){
+    assert(min != max);
+    if(min > max) {
+        int hold = max;
+        max = min;
+        min = hold;
+    }
     unsigned int seed = static_cast<unsigned int>(time(nullptr));
     srand(seed);
     return rand() % (max - min) + min;
