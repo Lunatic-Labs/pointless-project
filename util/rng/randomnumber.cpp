eefd77f39ed753629d20b3f4ea2e44257e1d46c2
connoree <connoreeaton@gmail.com>
2022-10-06 10:17:32 -0500

    Spacing

--- util/rng/randomnumber.cpp
+++ util/rng/randomnumber.cpp
@@ -4,11 +4,13 @@
 #include <cassert>
 
 int getNum(int min, int max){
+
     assert(min != max);
     if(min > max) {
         return getNum(max, min);
     }
     unsigned int seed = static_cast<unsigned int>(time(nullptr));
     srand(seed);
+    
     return rand() % (max - min) + min;
 }
