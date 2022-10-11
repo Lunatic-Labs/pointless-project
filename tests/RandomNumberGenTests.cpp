0b03ad09100ac9d459b2ad43f65aff4951f88e38
Justice <jwroberts1@mail.lipscomb.edu>
2022-10-11 14:00:54 -0500

    refactor: whitespace

--- tests/RandomNumberGenTests.cpp
+++ tests/RandomNumberGenTests.cpp
@@ -3,21 +3,26 @@
 #include <boost/test/unit_test.hpp>
 #include "../util/rng/RandomNumber.cpp"
 
-BOOST_AUTO_TEST_CASE(testGeneratorWorksWithLargeNumbers) {
+BOOST_AUTO_TEST_CASE(testGeneratorWorksWithLargeNumbers) 
+{
     int number = getNum(-1000000, 1000000);
     BOOST_ASSERT(number >= -1000000);
     BOOST_ASSERT(number <= 1000000);
 }
-BOOST_AUTO_TEST_CASE(testGeneratorWorksWithSmallNumbers) {
+
+BOOST_AUTO_TEST_CASE(testGeneratorWorksWithSmallNumbers) 
+{
     int number = getNum(0, 5);
     BOOST_ASSERT(number >= 0);
     BOOST_ASSERT(number <= 5);
 }
+
 BOOST_AUTO_TEST_CASE(testGeneratorWorksWithLargeNumbersInversed) {
     int number = getNum(1000000, -1000000);
     BOOST_ASSERT(number >= -1000000);
     BOOST_ASSERT(number <= 1000000);
 }
+
 BOOST_AUTO_TEST_CASE(testGeneratorWorksWithSmallNumbersInversed) {
     int number = getNum(5, 0);
     BOOST_ASSERT(number >= 0);
