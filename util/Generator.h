85da005728117114c867143739cf259240e8506b
Aldo Velarde <aldo.velarde2542001@gmail.com>
2022-10-13 11:21:14 -0500

    Added Generator.h function along with the HTML text file

--- util/Generator.h
+++ util/Generator.h
@@ -1,3 +1,39 @@
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
-std::string generateHtml(std::string title, std::string description, long seed);
+std::string generateHtml(std::string title, std::string description, long seed)
+{
+       // Stores the content of templateHeader into a string. 
+    char c;
+    string headerContent;
+    ifstream headerFile( "templateHeader.txt" );
+    headerFile >> noskipws;
+    while ( headerFile >> c )  headerContent += c;
+
+    // Stores the content of templateFooter into a string. 
+    char d;
+    string footerContent;
+    ifstream footerFile( "templateFooter.txt" );
+    footerFile >> noskipws;
+    while ( footerFile >> d ) footerContent += d;
+
+
+    string contentConcatenated = headerContent + "<h3 style=\"text-align:center\">" + title + "</h3>" + "<p style=\"text-align:center\">" + description + "</p>" + footerContent;
+    string templateSeed = to_string(seed) + "</div>\n" + "</div>\n" + "</div>\n" + "</body>\n" + "</html>\n";
+
+    headerFile.close();
+    footerFile.close();
+  
+    string fileName =  title + ".html";
+
+    //Creates and inserts content into a new file with the title as name.html
+    std::ofstream outfile (fileName);
+    outfile << contentConcatenated;
+    outfile << templateSeed;
+    outfile.close();
+
+    return fileName;
+};
