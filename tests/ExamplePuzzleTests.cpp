41fad4dce6f1ea20405e7f5e9ecc41f1eb5dffe1
Justice Roberts <jwroberts1@mail.lipscomb.edu>
2022-10-18 14:11:47 -0500

    Issue #23: Create Hello World Puzzle (#41)
    * HelloWorld Example Puzzle
    
    * Example Puzzle Testing
    
    * github action for tests
    
    Co-authored-by: connoree <connoreeaton@gmail.com>

---
+++ tests/ExamplePuzzleTests.cpp
@@ -0,0 +1,25 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE ExamplePuzzleTests
+#include <boost/test/unit_test.hpp>
+#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <zip.h>
+#include <stdio.h>
+#include "../puzzles/HelloWorld.cpp"
+
+BOOST_AUTO_TEST_CASE(testDoAllAttributesOfExamplePuzzleMakeSense)
+{
+    HelloWorld testPuzzle = HelloWorld();
+    std::string puzzleFile = testPuzzle.init("Example", "etc/congrats.zip");
+    BOOST_ASSERT(testPuzzle.getAnswer() == "4");
+    std::unique_ptr<int> errPtr = nullptr;
+    zip_t * testPuzzleZip = zip_open(puzzleFile.c_str(), 0, errPtr.get());
+    BOOST_ASSERT(!errPtr.get()); // Assert no errors in opening zip
+    BOOST_ASSERT(zip_fopen(testPuzzleZip, 
+                           (testPuzzle.getName() + ".html").c_str(), 
+                           0));
+    zip_close(testPuzzleZip);
+    BOOST_ASSERT(remove(puzzleFile.c_str()) == 0);
+    BOOST_ASSERT(remove((testPuzzle.getName() + ".html").c_str()) == 0);
+}
