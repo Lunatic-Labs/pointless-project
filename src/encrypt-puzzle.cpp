9d3c87ade1b6688187a5f82de13aae766c16e03d
malloc-nbytes <zacharydhaskins@yahoo.com>
2024-02-17 15:11:54 -0600

    Added ability to tweak puzzles by putting all of the  at the top of the file.

--- src/encrypt-puzzle.cpp
+++ src/encrypt-puzzle.cpp
@@ -5,23 +5,37 @@
 #include "./include/puzzle.h"
 #include "./include/utils.h"
 
-std::string words[] = {
-  "lipscomb university",
-  "computer science",
-  "programming languages",
-  "data structures",
-  "dijkstras algorithm",
-  "depth first search",
-  "breadth first search",
-  "graph theory",
-  "dynamic programming",
-  "greedy algorithms",
-  "big o notation",
-  "recursive function",
-  "binary search tree",
-};
+#define ENCR_WORDS {       \
+  "lipscomb university",   \
+  "computer science",      \
+  "programming languages", \
+  "data structures",       \
+  "dijkstras algorithm",   \
+  "depth first search",    \
+  "breadth first search",  \
+  "graph theory",          \
+  "dynamic programming",   \
+  "greedy algorithms",     \
+  "big o notation",        \
+  "recursive function",    \
+  "binary search tree",    \
+}
 
-#define WORDS_LEN (sizeof(words) / sizeof(words[0]))
+#define ENCR_OPS_SHIFT 0
+#define ENCR_OPS_SWAP 1
+#define ENCR_OPS_CHANGE 2
+
+#define ENCR_OPS {  \
+  ENCR_OPS_CHANGE,   \
+  ENCR_OPS_SWAP,     \
+  ENCR_OPS_SHIFT     \
+}
+
+#define ENCR_ROTATIONS_MIN 1 // Must be 1 or greater
+#define ENCR_ROTATIONS_MAX 3
+
+#define ENCR_CHANGE_MIN 1 // Must be 1 or greater
+#define ENCR_CHANGE_MAX 3
 
 void shift(std::string &s, int dir, int n)
 {
@@ -67,10 +81,11 @@ std::string num_to_english(int idx)
 
 Puzzle encrypt_puzzle_create(long seed)
 {
-  const int word_idx = utils_rng_roll(0, WORDS_LEN-1, seed);
+  std::string words[] = ENCR_WORDS;
+  const int word_idx = utils_rng_roll(0, sizeof(words)/sizeof(*words)-1, seed);
 
   std::string word = words[word_idx];
-  std::vector<int> ops = {2,1,0};
+  std::vector<int> ops = ENCR_OPS;
 
   std::vector<std::string> instructions;
 
@@ -80,7 +95,7 @@ Puzzle encrypt_puzzle_create(long seed)
     switch (ops[rng]) {
       case 0: {
         int dir = utils_rng_roll(0, 1, seed);
-        int rotations = utils_rng_roll(1, 3, seed);
+        int rotations = utils_rng_roll(ENCR_ROTATIONS_MIN, ENCR_ROTATIONS_MAX, seed);
         instructions.push_back(std::to_string(i+1) + ".) Shifted all characters by " + std::to_string(rotations) + " to the " + (dir ? "right" : "left") + ".\n");
         shift(word, dir, rotations);
       } break;
@@ -91,7 +106,7 @@ Puzzle encrypt_puzzle_create(long seed)
         swap(word, idx1, idx2);
       } break;
       case 2: {
-        int jump_dist = utils_rng_roll(1, 3, seed);
+        int jump_dist = utils_rng_roll(ENCR_CHANGE_MIN, ENCR_CHANGE_MAX, seed);
         instructions.push_back(std::to_string(i+1) + ".) Alphabetically increased every 4th character by " + std::to_string(jump_dist) + ".\n");
         change(word, jump_dist);
       } break;
