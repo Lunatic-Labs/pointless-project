b94a12b1fc1c8465af716e255e40f20d2dbe0658
TurnerAustin <notmoop@gmail.com>
2024-02-20 18:53:51 -0600

    Adjusted encrypt to be harder, clearer and easier to adjust later.

--- src/encrypt-puzzle.cpp
+++ src/encrypt-puzzle.cpp
@@ -26,9 +26,11 @@
 #define ENCR_OPS_CHANGE 2
 
 #define ENCR_OPS {  \
-  ENCR_OPS_CHANGE,   \
-  ENCR_OPS_SWAP,     \
-  ENCR_OPS_SHIFT     \
+  ENCR_OPS_CHANGE,  \
+  ENCR_OPS_SWAP,    \
+  ENCR_OPS_SWAP,    \
+  ENCR_OPS_SWAP,    \
+  ENCR_OPS_SHIFT,   \
 }
 
 #define ENCR_ROTATIONS_MIN 1 // Must be 1 or greater
@@ -85,37 +87,39 @@ Puzzle encrypt_puzzle_create(long seed)
   const int word_idx = utils_rng_roll(0, sizeof(words)/sizeof(*words)-1, seed);
 
   std::string word = words[word_idx];
-  std::vector<int> ops = ENCR_OPS;
+  size_t len = word.size()-1;
 
-  std::vector<std::string> instructions;
+  std::vector<int> ops = ENCR_OPS;
+  std::string encrypt_steps = "";
 
   for (int i = 0; ops.size(); ++i) {
     int rng = utils_rng_roll(0, ops.size()-1, seed);
-    size_t len = word.size()-1;
+    encrypt_steps += "<li>";
     switch (ops[rng]) {
       case 0: {
         int dir = utils_rng_roll(0, 1, seed);
         int rotations = utils_rng_roll(ENCR_ROTATIONS_MIN, ENCR_ROTATIONS_MAX, seed);
-        instructions.push_back(std::to_string(i+1) + ".) Shifted all characters by " + std::to_string(rotations) + " to the " + (dir ? "right" : "left") + ".\n");
+        encrypt_steps += std::to_string(i+1) + ".) Shifted all characters by " + std::to_string(rotations) + " to the " + (dir ? "right" : "left") + ".\n";
         shift(word, dir, rotations);
       } break;
       case 1: {
         int idx1 = utils_rng_roll(0, len, seed);
         int idx2 = utils_rng_roll(0, len, seed);
-        instructions.push_back(std::to_string(i+1) + ".) Swapped the " + num_to_english(idx1+1) + " and " + num_to_english(idx2+1) + " characters.\n");
+        encrypt_steps += std::to_string(i+1) + ".) Swapped the " + num_to_english(idx1+1) + " and " + num_to_english(idx2+1) + " characters.\n";
         swap(word, idx1, idx2);
       } break;
       case 2: {
         int jump_dist = utils_rng_roll(ENCR_CHANGE_MIN, ENCR_CHANGE_MAX, seed);
-        instructions.push_back(std::to_string(i+1) + ".) Alphabetically increased every 4th character by " + std::to_string(jump_dist) + ".\n");
+        encrypt_steps += std::to_string(i+1) + ".) Alphabetically increased every 4th letter by " + std::to_string(jump_dist) + ".\n";
         change(word, jump_dist);
       } break;
       default:
         assert(false && "Invalid operation");
     }
+    encrypt_steps += "</li>\n";
     ops.erase(ops.begin() + rng);
   }
-  instructions.insert(instructions.begin(), word);
+  std::vector<std::string> instructions { word, encrypt_steps };
 
   Puzzle p{"files-encrypt", words[word_idx]};
 
