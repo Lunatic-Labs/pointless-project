a6a776f7bb9e88cc1795f03a0d2c2b03bb1f9abc
malloc-nbytes <zacharydhaskins@yahoo.com>
2024-02-28 22:51:46 -0600

    binary addition puzzle is done

--- src/binary-addition-puzzle.cpp
+++ src/binary-addition-puzzle.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <bitset>
 #include <cmath>
+#include <random>
+#include <algorithm>
 
 #include "./include/utils.h"
 #include "./include/puzzle.h"
@@ -9,22 +11,47 @@
 #define TAPE_WIDTH 10
 #define TAPE_HEIGHT 3
 
+#define REQ_CARRIES 3
+
 #define EMPTY {255, 255, 255} // blank
 #define RED {255, 0, 0}       // off
 #define GREEN {0, 255, 0}     // on
 #define GOLD {255, 255, 0}    // start
 #define PURPLE {255, 0, 255}  // end
 
+std::string create_bin_str(long seed)
+{
+  std::string bin;
+  for(size_t i = 0; i < TAPE_WIDTH; ++i) {
+    if (i < (TAPE_WIDTH/2)) {
+      bin += '1';
+    } else {
+      bin += '0';
+    }
+  }
+  std::shuffle(bin.begin(), bin.end(), std::default_random_engine(seed));
+  return bin;
+}
+
 Puzzle binary_addition_puzzle_create(long seed)
 {
-  // TODO: make sure not 0 and not equal
-  int top = utils_rng_roll(0, std::pow(2, TAPE_WIDTH), seed);
-  int bottom = utils_rng_roll(0, std::pow(2, TAPE_WIDTH), seed);
-  std::cout << top << " " << bottom << std::endl;
 
-  std::string top_bin = std::bitset<TAPE_WIDTH>(top).to_string();
-  std::string bottom_bin = std::bitset<TAPE_WIDTH>(bottom).to_string();
-  std::string sum_bin = std::bitset<TAPE_WIDTH+1>(top + bottom).to_string();
+  std::string top;
+  std::string bottom;
+
+  int carries = 0;
+  while (carries < REQ_CARRIES) {
+    top = create_bin_str(seed+=92);
+    bottom = create_bin_str(seed+=2);
+    carries = 0;
+    for(size_t i = 0; i < TAPE_WIDTH; ++i) {
+      if (top[i] == '1' && bottom[i] == '1') {
+        ++carries;
+      }
+    }
+  }
+
+  std::string sum = std::bitset<TAPE_WIDTH+1>(stoi(top, nullptr, 2) + std::stoi(bottom, nullptr, 2)).to_string();
 
   Image graph(TAPE_WIDTH+2, TAPE_HEIGHT);
 
@@ -38,12 +65,12 @@ Puzzle binary_addition_puzzle_create(long seed)
   graph(0,TAPE_WIDTH+1) = GOLD;
 
   for (size_t i = 0; i < TAPE_WIDTH; ++i) {
-    if (top_bin[i] == '1') {
+    if (top[i] == '1') {
       graph(0,i+1) = GREEN;
     } else {
       graph(0,i+1) = RED;
     }
-    if (bottom_bin[i] == '1') {
+    if (bottom[i] == '1') {
       graph(1,i+1) = GREEN;
     } else {
       graph(1,i+1) = RED;
@@ -54,5 +81,5 @@ Puzzle binary_addition_puzzle_create(long seed)
   std::string html_body = utils_html_printf("Graph Paper Robot", "./files-binary-addition/.desc.txt", {{svg}});
   utils_generate_file("./files-binary-addition/instructions.html", html_body);
 
-  return Puzzle{"files-binary-addition", sum_bin, {}};
+  return Puzzle{"files-binary-addition", sum, {}};
 }
