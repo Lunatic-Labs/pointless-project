8f582c69263e4bf4635114e2c6f03dd1a97eff2a
Mekeal <brownmekeal@gmail.com>
2024-03-27 17:32:51 -0500

    Moved some magic numbers to #define

--- src/based-puzzle.cpp
+++ src/based-puzzle.cpp
@@ -3,14 +3,19 @@
 #include "./include/puzzle.h"
 #include "./include/utils.h"
 
+#define CELLS 12
+#define RAND_MAX 67 //0-42, A-Z(0, 67)
 
-////// STEPS TO SOLVE //////
-//Convert values to base 10 
-// Sum them up for each row 
-// Convert the sum to base 2 
-// Check if lsb is on or off 
-// Save the resulting lsb values 
-// Take that base 2 number and convert it back to base 10 
+
+/*
+* * * * STEPS TO SOLVE * * * *
+* Convert values to base 10 
+* Sum them up for each row 
+* Convert the sum to base 2 
+* Check if lsb is on or off 
+* Save the resulting lsb values 
+* Take that base 2 number and convert it back to base 10 
+*/
 int solve(std::vector<std::string> &vals, std::vector<int> &base)
 {
   int decimalval, items, solution, mask;
@@ -33,28 +38,27 @@ int solve(std::vector<std::string> &vals, std::vector<int> &base)
   return solution;
 }
 
-//TODO: Rework the puzzle
 Puzzle based_puzzle_create(long seed)
 {
   std::vector<std::string> values;
   std::vector<int> bases;
 
-  for (int i = 0; i < 12; i++) {
-    short rand = utils_rng_roll(0, 67, seed);
-    if (rand > 42) { // Keep them within hex range of values
+  for (int i = 0; i < CELLS; i++) {
+    short rand = utils_rng_roll(0, RAND_MAX, seed);
+    if (rand > 42) { // Keep them within hex range of values, 42-47 == A-F
       if (rand > 47) {
         rand = utils_rng_roll(42, 47, seed);
       }
 
-      values.push_back(std::string(1, static_cast<char>(rand + 23)));
+      values.push_back(std::string(1, static_cast<char>(rand + 23))); // + 23 to get correct ASCII value
       bases.push_back(16);
     } else {
       int base;
 
-      if (rand >= 0 && rand <= 20) {
+      if (rand >= 0 && rand <= 20) { //0-20 is base 20
         base = 20;
       }
-      else if (rand >= 21 && rand <= 42) {
+      else if (rand >= 21 && rand <= 42) { //21-42 is base 31
         base = 31;
       }
 
