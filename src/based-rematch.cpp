780c0c48b1286d19882d8cdffc12d7b90d971938
Mekeal <brownmekeal@gmail.com>
2024-03-30 17:19:37 -0500

    Based rematch puzzle nearly complete -needs some prettying..is that a word?..up -needs more testing to ensure that the sorting is actually working -code could definitly use some cleaning up

--- src/based-rematch.cpp
+++ src/based-rematch.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 #include "./include/puzzle.h"
 #include "./include/utils.h"
 
 //define puzzle sizes
-#define GRID_CELLS 11
-#define CELL_SZ 4
+#define NUM_VALUES 11
+#define VAL_SZ 4
 
 
 //define indexes into values vector based on the base
@@ -21,9 +22,10 @@
 
 
 
-static const std::vector<std::string> values = {"!", "@", "#", "$", "%", "^", "&", "*", "{", "}", "[", "]", \
-    "(", ")", "'\'", ">", "a", "b", "c", "d", "e", "f", "g", "h", "|", "~", ",", "/", "?", "_", "+", "=", "-", \
+static const std::vector<std::string> base_nums = {"!", "@", "#", "$", "%", "^", "&", "*", "{", "}", "[", "]", \
+    "(", ")", "`", ">", "a", "b", "c", "d", "e", "f", "g", "h", "|", "~", ",", "/", "?", "_", "+", "=", "-", \
     ":", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
 
 
 /*
@@ -50,12 +52,58 @@ int solve(std::vector<std::string> &vals, std::vector<int> &base)
 }
 */
 
+//void sort(strvec_t )
 
-int cell_solve(strvec_t vals, std::vector<int> base);
 
+//return the index of the symbol in relation to its base
+int find_val(int base, std::string symbol)
+{
+  int start, end;
+  switch(base)
+  {
+    case 8:
+      start = S8_START;
+      end = S8_END;
+      break;
+    case 16:
+      start = G16_START;
+      end = G16_END;
+      break;
+    case 36:
+      start = M36_START;
+      end = M36_END;
+      break;
+  }
+  int i = 0;
+  for(start; start <= end; start++) {
+    if(base_nums[start] == symbol) {
+      return i;
+    }
+    ++i;
+  }
+  std::cout << "didnt find value...\n\n";
+  return -1;
+}
 
-//creates value that will be placed in a cell in the table
-void cells_create(const strvec_t values_str, const std::vector<int> bases, strvec_t *cells, long seed)
+void values_solve(strvec_t vals, std::vector<int> bases, std::vector<int> *solved)
+{
+  for(int i = 0; i < NUM_VALUES; i++) {
+    int sol = 0;
+    std::vector<int> pos;
+    for(int j = 0; j < 4; j++) {
+      pos.push_back(find_val(bases[i], std::string(1, vals[i][j]))); //values index in its list of values
+    }
+
+    int pow = 3;
+    for(int j = 0; j < 4; j++) {
+      sol += pos[j] * std::pow(bases[i], pow--);
+    }
+    solved->push_back(sol);
+  }
+}
+
+//creates a value, 4 numbers from a chosen base
+void values_create(const std::vector<int> bases, strvec_t *values, long seed)
 {
   for(auto it = bases.begin(); it != bases.end(); ++it) {
     auto& v = *it;
@@ -76,21 +124,20 @@ void cells_create(const strvec_t values_str, const std::vector<int> bases, strve
         end = M36_END;
         break;
     }
-    for(int i = 0; i < CELL_SZ; i++) {
+    for(int i = 0; i < VAL_SZ; i++) {
       int num = utils_rng_roll(start, end, seed);
-      cell_val += values_str[num];
+      cell_val += base_nums[num];
     }
-    //std::cout << "cell val = " << cell_val << "\n";
-    cells->push_back(cell_val);
+    values->push_back(cell_val);
   }
 }
 
 
 Puzzle based_rematch_puzzle_create(long seed)
 {
-  std::vector<int> bases; 
-  strvec_t grid;
-  for(int i = 0; i < GRID_CELLS; i++) {
+  std::vector<int> bases, solved; 
+  strvec_t values;
+  for(int i = 0; i < NUM_VALUES; i++) {
     int num = utils_rng_roll(0, 2, seed);
     switch (num) //choose base based...nice...on random value
     {
@@ -105,12 +152,44 @@ Puzzle based_rematch_puzzle_create(long seed)
         break;
     }
   }
-  cells_create(values, bases, &grid, seed);
+  values_create(bases, &values, seed);
+  values_solve(values, bases, &solved);
 
-  std::string html_content = utils_html_printf("Base Puzzle Rematch", "./files-based-r/.desc.txt", grid);
+  int iter = 0;
+  for(std::string s: values) {
+    std::cout << s << "\n";
+  }
+  std::cout << "\n";
+
+  //TODO: VERIFY THAT THIS SORTING IS ACCURATE
+  //sort values from smallest to greatest
+  std::vector<std::pair<int, std::string>> paired_vec;
+  for(size_t i = 0; i < solved.size(); i++) {
+    paired_vec.push_back(std::make_pair(solved[i], values[i]));
+  }
+  std::sort(paired_vec.begin(), paired_vec.end());
+  for (const auto& pair : paired_vec) {
+      values.push_back(pair.second);
+  }
+
+  int key = utils_rng_roll(10000, 150000, seed);
+  values.push_back(std::to_string(key));
+
+  //debug purposes ofc
+  iter = 0;
+  for(std::string s: values) {
+    if(iter++ <= 10)
+      continue;
+    std::cout << s << "\n";
+  }
+  std::cout << "\n";
+
+
+
+  std::string html_content = utils_html_printf("Base Puzzle Rematch", "./files-based-r/.desc.txt", values);
   utils_generate_file("./files-based-r/instructions.html", html_content);
 
-  return Puzzle{"files-based-r", "need to finish solve", {}};
+  return Puzzle{"files-based-r", values.back(), {}};
 }
 
 /*TODO: maybe have the puzzle not require all of the bases to be converted. 
@@ -142,4 +221,12 @@ Puzzle based_rematch_puzzle_create(long seed)
 *        3 symbols of same base per table box
 *        hidden elements on page that are only visible when the user hovers over it(little hints maybe?)
 *        clicking on the highlighted element will cause the hint text to appear(probably just the different bases' symbols enumerated)
+*/
+
+
+
+/*
+* You have found yourself in a dark dungeon. There is a mysterious glowing obelisk in the center of the room.
+* In order to leave this dungeon, the obelisk must be filled, but not in any old fashion.
+* Your challenge is quite base-ic, really. Collect the pieces strewn about the room to fill the obelisk. 
 */
