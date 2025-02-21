c5c938251c1cba5f3ffec43e2cdc582af34cdaff
M-Angelo0 <mike20hernandez03@gmail.com>
2025-02-21 03:34:14 +0000

    V2 of John's SCRUM

---
+++ web-server/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <main>
+        <form action="includes/formhandler.php" method="post">
+            <label for="firstname">Firstname?</label>
+            <input id="firstname" type="text" name="firstname" placeholder="Firstname...">
+
+            <label for="lastname">Lastname?</label>
+            <input id="lastname" type="text" name="lastname" placeholder="Lastname...">
+
+            <button type="submit" name="submit">Submit</button>
+        </form>
+    </main>
+
+    </body>
+</html>
