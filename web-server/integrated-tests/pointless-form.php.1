8376162c3af331222b24032801063a66fb40fc8a
johntable <john.m.tabelisma@gmail.com>
2025-03-25 10:22:50 -0500

    merge php w/ index; other testing

--- web-server/integrated-tests/pointless-form.php.1
+++
@@ -1,2 +0,0 @@
-<script>alert('!! INVALID EMAIL !! Please enter a vaild email...')
-    window.location.href='../index.html'</script>
--- web-server/integrated-tests/test-duplicate-user.sh
+++ web-server/integrated-tests/test-duplicate-user.sh
@@ -4,3 +4,4 @@ cp ~/Desktop/pointless-project/web-server/includes/contact-data.csv ~/Desktop/po
 wget --post-data 'usr_fname=j&usr_lname=j&usr_email=j@j.com' http://localhost:8000/includes/pointless-form.php
 
 diff ~/Desktop/pointless-project/web-server/includes/contact-data.csv ~/Desktop/pointless-project/web-server/integrated-tests/ideal-csv/test-duplicate-user.csv
+rm ideal-csv/test-duplicate-user.csv
