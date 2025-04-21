5b73e247b09921a7e5425e98cfcdddda2f72a687
johntable <john.m.tabelisma@gmail.com>
2025-04-21 18:14:14 -0500

    updated testing naming conventions in README.org

--- web-server/integrated-tests/test-index-GET.sh
+++ web-server/integrated-tests/test-index-GET.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 cd ..
 current_datetime=$(date +"%Y-%m-%d_%H:%M:%S")
-touch ./integrated-tests/output_$current_datetime.txt
+touch ./integrated-tests/test-index-GET_output_$current_datetime.txt
 
 # GET to retrieve index page and put into txt file
-wget http://localhost:8000/index.php -O ./integrated-tests/output_$current_datetime.txt
+wget http://localhost:8000/index.php -O ./integrated-tests/test-index-GET_output_$current_datetime.txt
 
 # Retreived webpage vs Expetcted webpage
-if ! grep -q '<input type="text" id="fname" name="fname" required>' ./integrated-tests/output_$current_datetime.txt; then
+if ! grep -q '<input type="text" id="fname" name="fname" required>' ./integrated-tests/test-index-GET_output_$current_datetime.txt; then
     echo 'Error: No First Name input element.'
-elif ! grep -q '<input type="text" id="lname" name="lname" required>' ./integrated-tests/output_$current_datetime.txt; then
+elif ! grep -q '<input type="text" id="lname" name="lname" required>' ./integrated-tests/test-index-GET_output_$current_datetime.txt; then
     echo 'Error: No Last Name input element.'
-elif ! grep -q '<input type="text" id="email" name="email" required>' ./integrated-tests/output_$current_datetime.txt; then
+elif ! grep -q '<input type="text" id="email" name="email" required>' ./integrated-tests/test-index-GET_output_$current_datetime.txt; then
     echo 'Error: Email input element.'
-elif ! grep -q '<button type="submit">Submit</button>' ./integrated-tests/output_$current_datetime.txt; then
+elif ! grep -q '<button type="submit">Submit</button>' ./integrated-tests/test-index-GET_output_$current_datetime.txt; then
     echo 'Error: No submit button.'
 else
     # Delete wget output, if successful
     echo 'No Errors. Elements present!'
-    rm ./integrated-tests/output_$current_datetime.txt
+    rm ./integrated-tests/test-index-GET_output_$current_datetime.txt
 fi
--- web-server/integrated-tests/test-index-POST.sh
+++ web-server/integrated-tests/test-index-POST.sh
@@ -2,11 +2,11 @@
 cd ..
 current_datetime=$(date +"%Y-%m-%d_%H:%M:%S")
 echo $current_datetime
-touch ./integrated-tests/output_$current_datetime.txt
+touch ./integrated-tests/test-index-POST_output_$current_datetime.txt
 cp ./includes/contact-data.csv ./integrated-tests/original-data_$current_datetime.csv
 
 # Send POST request to index.php
-wget --post-data "fname=j&lname=j&email=j@j.com" http://localhost:8000/index.php -O ./integrated-tests/output_$current_datetime.txt
+wget --post-data "fname=j&lname=j&email=j@j.com" http://localhost:8000/index.php -O ./integrated-tests/test-index-POST_output_$current_datetime.txt
 
 # Check for if input went through, delete output txt if successful
 if ! grep 'j,j,j@j.com' ./includes/contact-data.csv; then
@@ -25,4 +25,4 @@ else
 fi
 
 # Delete output txt
-rm ./integrated-tests/output_$current_datetime.txt
+rm ./integrated-tests/test-index-POST_output_$current_datetime.txt
